4b3d1bf0c562f46592c1f2e745f778d922ff1fd2
victorlms <victor.lms00@gmail.com>
2017-01-24 11:16:49 -0200

    Criação Prefab Grid

---
+++ Lacto Defender/Assets/Script/movimentoPadraoPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentoPadraoPlayer : MonoBehaviour {
+
+	List<GameObject> line;
+
+
+
+	void Start () {
+
+
+
+	}//FECHA_START
+	
+
+	void Update () {
+
+		if (Input.GetTouch (0).phase == TouchPhase.Began) {
+
+		}
+
+	}//FECHA_UPDATE
+}
