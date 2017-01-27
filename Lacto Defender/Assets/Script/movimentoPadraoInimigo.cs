311ea079bc02089dcb27bb0ad23a1842b2376e10
GuilhermeEnnes <jaksonguilherme2@gmail.com>
2017-01-27 04:40:58 -0200

    caminhar dinâmico do inimigo feito. Reconhecimento da vaca pelo alien e fase de raciocinio.

--- Lacto Defender/Assets/Script/movimentoPadraoInimigo.cs
+++ Lacto Defender/Assets/Script/movimentoPadraoInimigo.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class movimentoPadraoInimigo : MonoBehaviour 
 {
-	void Start(){}
-	// Update is called once per frame
+	public float Caminhada;
+	public int num_de_colisões;
+	public List<GameObject> linhas;
+	public GameObject espaços;
+
+	//void start ()
+	//{
+	//	linhas = new List<GameObject> ();
+	//	espaços = gameObject.transform.GetChild (1).gameObject;
+	//	linhas.Add(espaços);
+	//}
+
 	void Update ()
 	{
-		
+		transform.Translate (Vector3.left * Caminhada * Time.deltaTime);
+
+	
 	}
+	void OnTriggerStay2D(Collider2D campo)
+	{
+	
+		if (campo.gameObject.tag == "Box7") {
+			num_de_colisões= num_de_colisões+1;
+		}
+	
+	}
+
 }
