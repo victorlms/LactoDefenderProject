dc61e70768ef1be33fab97ba666cd9456ef5924d
GuilhermeEnnes <jaksonguilherme2@gmail.com>
2017-01-30 16:38:18 -0200

    criação script statusInimigo parar agrupar valores relevantes sobre o inimigo

---
+++ Lacto Defender/Assets/Script/StatusInimigo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusInimigo : MonoBehaviour {
+
+// FUNÇÃO DESSE SCRIPT É SER, A PRINICIPIO, APENAS UM DEPÓSITO DE INFORMAÇÃO SOBRE O INIMIGO
+
+	public int life;
+	public int damage;
+	public int speed;
+
